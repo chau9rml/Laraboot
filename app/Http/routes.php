df1e46858ae956dc0fec3995bb96a90c13d7dcd1
Le Vu Minh Chau <minh.chau@slopetracker.net>
2016-03-19 21:06:57 +0700

    Create Routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,5 +27,20 @@ Route::get('/', function () {
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+
+    //Authentication Route
+
+    Route::get('auth/login', 'Auth\AuthController@getLogin');
+    Route::post('auth/login', 'Auth\AuthController@postLogin');
+    Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+    //Registration Route
+
+    Route::get('auth/register', 'Auth\AuthController@getRegister');
+    Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+    //Forget password Route
+    Route::controllers([
+        'password' => 'Auth\passwordController'
+    ]);
 });
