c51873f75d14905848d054412c261fd4547aa70d
Le Vu Minh Chau <minh.chau@slopetracker.net>
2016-03-20 19:30:41 +0700

    Simple Widget Datatable

---
+++ resources/views/widget/datatable.blade.php
@@ -0,0 +1,14 @@
+<div class="baseMargin">
+    <table id="widget_table" class="display">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Date Created</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+</div>
