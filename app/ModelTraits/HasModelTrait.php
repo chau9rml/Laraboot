3eb6780cca26f992f8defd52c2e4b2a077edc292
Le Vu Minh Chau <minh.chau@slopetracker.net>
2016-03-20 21:28:33 +0700

    create ModelTrail using Carbon to format date time

---
+++ app/ModelTraits/HasModelTrait.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\ModelTraits;
+
+use Carbon\Carbon;
+
+trait HasModelTrait
+{
+    public function showDateCreated($createdAtTimestamp)
+    {
+        return Carbon::parse($createdAtTimestamp)->format('m/d/Y');
+    }
+
+}
